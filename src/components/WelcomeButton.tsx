ab275e78ed97c2fe9fc8b455af56996b55adf62d
dvargas42 <dvargas.eng@gmail.com>
2021-05-12 16:05:00 -0300

    welcome button creation

---
+++ src/components/WelcomeButton.tsx
@@ -0,0 +1,35 @@
+import React, { ReactElement, ReactNode } from 'react';
+import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native'
+
+import colors from '../styles/colors'
+
+interface ButtonProps extends TouchableOpacityProps {
+  
+  children: ReactNode;
+}
+
+export function WelcomeButton({ children, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity 
+      style={styles.button}
+      activeOpacity={0.8}
+      {...rest}
+    >
+      <Text>
+        {children}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+})
