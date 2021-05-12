fa5744a680d19ccaee64db08468a5f9e7269778a
dvargas42 <dvargas.eng@gmail.com>
2021-05-12 16:01:27 -0300

    fonts and routes implementation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 
-import { Welcome } from './src/pages/Welcome';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from '@expo-google-fonts/jost';
+
+import Routes from './src/routes';
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+
+  if (!fontsLoaded)
+    return <AppLoading />
+    
   return (
     <>
-      <Welcome />
       <StatusBar style="auto" /> 
+      <Routes/>
     </>
   );
 }
