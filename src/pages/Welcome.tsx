3cdcd4c37a4055ac091e4de7fa64de3848440747
dvargas42 <dvargas.eng@gmail.com>
2021-05-12 16:23:14 -0300

    welcome page changing

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,28 +1,40 @@
 import React from 'react'
-import { Image, Platform, SafeAreaView, StyleSheet, Text, TouchableOpacity } from  'react-native'
+import { Dimensions, Image, Platform, SafeAreaView, StyleSheet, Text, View } from  'react-native'
+import { Feather } from '@expo/vector-icons'
 
 import wateringImg from '../assets/watering.png'
-import { Button } from '../components/Button'
+import { WelcomeButton } from '../components/WelcomeButton'
 
-import { colors } from '../styles/colors'
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}suas plantas de {'\n'}forma fácil
-      </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}suas plantas de{'\n'}forma fácil
+        </Text>
 
-      <Image style={styles.image} source={wateringImg}/>
+        <Image 
+          style={styles.image}
+          source={wateringImg}
+          resizeMode="contain"
+        />
 
-      <Text style={styles.subtitle}>
-        Não esqueça maid e regar suas plantas.
-        Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas.
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
 
-      <Button title={'>'} />
-
+        <WelcomeButton>
+          <Feather
+            name="chevron-right"
+            style={styles.buttonIcon}
+          />
+        </WelcomeButton>
+      </View>
     </SafeAreaView>
   )
 }
@@ -30,25 +42,40 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingTop: Platform.OS === 'android' ? 25 : 0
+    paddingTop: Platform.OS === 'android' ? 10 : 0
   },
+
+  wrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginHorizontal: 10,
+  },
+
   title: {
-    fontSize: 32,
-    fontWeight: 'bold',
+    fontSize: 28,
+    //fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
+    fontFamily: fonts.heading,
     marginTop: 38,
+    lineHeight: 34,
   },
+
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.heading,
   },
+
   image: {
-    width: 292,
-    height: 284
+    height: Dimensions.get('window').width * 0.7
   },
+
+  buttonIcon: {
+    fontSize: 32,
+    color: colors.white,
+  }
 })
