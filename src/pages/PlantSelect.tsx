f8ef78c0778ea400820089ef8067515dba796459
dvargas42 <dvargas.eng@gmail.com>
2021-05-13 23:26:56 -0300

    plant selection page creation

---
+++ src/pages/PlantSelect.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import {
+  FlatList,
+  Image,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+
+import { Header } from '../components/Header'
+import { EnvironmentButton } from '../components/EnvironmentButton';
+
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
+export function PlantSelect() {
+  return (
+    <SafeAreaView style={styles.constainer}>
+      <Header />
+
+      <View style={styles.content}>
+        <Text
+          style={styles.title}
+        >
+          Em qual ambiente
+        </Text>
+        <Text
+          style={styles.subtitle}
+        >
+          você quer colocar sua planta?
+        </Text>
+      </View>
+
+      <View>
+        <FlatList 
+          data={[1,2,3,4,5,6]}
+          renderItem={() => (
+            <EnvironmentButton
+              title="Cozinha"
+              active
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.environmentList}
+        />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  constainer: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? 20 : 0,
+    backgroundColor: colors.background
+  },
+
+  content: {
+    marginTop: 40,
+    paddingHorizontal: 30,
+  },
+
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+
+  subtitle: {
+    fontSize: 17,
+    fontFamily: fonts.text,
+    lineHeight: 20,
+    color: colors.heading,
+  },
+
+ environmentList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginTop: 32
+  }
+})
