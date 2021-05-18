7ca36f26d8084612b6a7de9428c54e4842a52496
dvargas42 <dvargas.eng@gmail.com>
2021-05-18 20:59:41 -0300

    dynamic data implementation

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigation } from '@react-navigation/core';
+import { useNavigation, useRoute } from '@react-navigation/core';
 import {
   Platform,
   SafeAreaView,
@@ -13,32 +13,53 @@ import { Button } from '../components/Button'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
+interface Params {
+  title: string;
+  subtitle: string;
+  buttonTitle: string;
+  icon: 'smile' | 'hug';
+  nextScreen: string;
+}
+
+const emojis = {
+  hug: '🤗',
+  smile: '😄'
+}
+
 export function Confirmation() {
   const navigation = useNavigation()
+  const routes = useRoute()
+
+  const {
+    title,
+    subtitle,
+    buttonTitle,
+    icon,
+    nextScreen,
+  } = routes.params as Params
 
   function handleMoveOn() {
-    navigation.navigate("PlantSelect")
+    navigation.navigate(nextScreen)
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.emoji}>
-          😄️
+          {emojis[icon]}
         </Text>
 
         <Text style={styles.title}>
-          Prontinho!
+          {title}
         </Text>
 
         <Text style={styles.subtitle}>
-          Agora vamos começar a cuidar das suas
-          plantinhas com muito carinho.
+          {subtitle}
         </Text>
 
         <View style={styles.footer}>
           <Button
-            title="Começar"
+            title={buttonTitle}
             onPress={handleMoveOn}
           />
         </View>
