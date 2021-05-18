7fe9b89f48fec8618693734e81cad18f1f2f0a9c
dvargas42 <dvargas.eng@gmail.com>
2021-05-18 20:53:37 -0300

    code linting

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
+import Routes from './src/routes';
 
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold,
 } from '@expo-google-fonts/jost';
-
-import Routes from './src/routes';
-
 
 export default function App() {
   const [fontsLoaded] = useFonts({
