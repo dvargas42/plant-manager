d622c002644ca5d83f0a4b672642370e976bc973
dvargas42 <dvargas.eng@gmail.com>
2021-05-20 17:08:14 -0300

    notifications listening implementation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppLoading from 'expo-app-loading';
+import * as Notifications from 'expo-notifications'
 import { StatusBar } from 'expo-status-bar';
+
 import Routes from './src/routes';
 
 import {
@@ -8,12 +10,34 @@ import {
   Jost_400Regular,
   Jost_600SemiBold,
 } from '@expo-google-fonts/jost';
+import { PlantProps } from './src/libs/storage';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
   })
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async (notification) => {
+        const data = notification.request.content.data.plant as PlantProps
+        console.log(data)
+    })
+
+    return () => subscription.remove()
+
+    // async function notifications() {
+    //   await Notifications.cancelAllScheduledNotificationsAsync()
+
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log('######## Notificações Agendadas ###############')
+    //   console.log(data)
+
+    // }
+
+    // notifications()
+  }, [])
 
   if (!fontsLoaded)
     return <AppLoading />
